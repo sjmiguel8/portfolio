d8edaa7f4fc218ef2e65a7c3516d807b540a27a5
SJMiguel8 <bonilla.miguel.e@gmail.com>
2025-01-23 23:18:49 -0600

    better design

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -6,60 +6,24 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import { makeStyles } from '@mui/styles';
 import '../styles/Home.css';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    margin: '20px auto',
-  },
-  media: {
-    height: 140,
-  },
-  home: {
-    textAlign: 'center',
-    padding: '20px',
-  },
-  projects: {
-    marginTop: '40px',
-  },
-  projectItem: {
-    marginBottom: '20px',
-  },
-  appBar: {
-    backgroundColor: '#333', // Change AppBar background color
-  },
-  title: {
-    color: '#ffcc00', // Change title color
-  },
-  button: {
-    backgroundColor: '#ffcc00', // Change button background color
-    color: '#333', // Change button text color
-    '&:hover': {
-      backgroundColor: '#ff9900', // Change button hover color
-    },
-  },
-});
-
 const Home = () => {
-  const classes = useStyles();
-
   return (
     <div>
-      <AppBar position="static" className={classes.appBar}>
+      <AppBar position="static" className="appBar">
         <Toolbar>
-          <Typography variant="h6" component="div" className={classes.title} sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" className="title" style={{ flexGrow: 1 }}>
             My Professional Portfolio
           </Typography>
-          <Button variant="contained" className={classes.button}>
+          <Button variant="contained" className="button">
             Click Me
           </Button>
         </Toolbar>
       </AppBar>
-      <Card className={classes.root}>
+      <Card className="root">
         <CardMedia
-          className={classes.media}
+          className="media"
           image="/static/images/cards/contemplative-reptile.jpg"
           title="Random Image"
         />
@@ -72,19 +36,19 @@ const Home = () => {
           </Typography>
         </CardContent>
       </Card>
-      <div className={classes.home}>
+      <div className="home">
         <Typography variant="h3" component="h1">
           Welcome to My Professional Portfolio
         </Typography>
         <Typography variant="body1" component="p">
           Explore my projects and learn more about me!
         </Typography>
-        <div className={classes.projects}>
+        <div className="projects">
           <Typography variant="h4" component="h2">
             Featured Projects
           </Typography>
           <ul>
-            <li className={classes.projectItem}>
+            <li className="projectItem">
               <Typography variant="h5" component="h3">
                 Project One
               </Typography>
@@ -92,7 +56,7 @@ const Home = () => {
                 Description of project one.
               </Typography>
             </li>
-            <li className={classes.projectItem}>
+            <li className="projectItem">
               <Typography variant="h5" component="h3">
                 Project Two
               </Typography>
@@ -100,7 +64,7 @@ const Home = () => {
                 Description of project two.
               </Typography>
             </li>
-            <li className={classes.projectItem}>
+            <li className="projectItem">
               <Typography variant="h5" component="h3">
                 Project Three
               </Typography>
@@ -109,14 +73,6 @@ const Home = () => {
               </Typography>
             </li>
           </ul>
-        </div>
-        <div className="about-me">
-          <Typography variant="h4" component="h2">
-            About Me
-          </Typography>
-          <Typography variant="body1" component="p">
-            Hello, I'm Miguel Bonilla, a dedicated developer with extensive experience in web development. I am passionate about creating highly functional and efficient websites.
-          </Typography>
         </div>
       </div>
     </div>
