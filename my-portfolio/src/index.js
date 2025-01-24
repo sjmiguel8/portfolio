207179f166dea2c9b4cfc80bd83eecd0b6b34091
SJMiguel8 <bonilla.miguel.e@gmail.com>
2025-01-24 00:06:38 -0600

    better design

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/App.css';
+import './index.css'; // Ensure this import is correct
+import Particles from 'particles.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  Particles.init({
+    selector: '.background',
+    color: '#ffffff',
+    connectParticles: true,
+    maxParticles: 100,
+  });
+});
 
 ReactDOM.render(
   <React.StrictMode>
