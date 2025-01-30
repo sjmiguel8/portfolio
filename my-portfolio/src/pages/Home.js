b919badbd7801f1ee5d050b25acb8980bb619c11
SJMiguel8 <bonilla.miguel.e@gmail.com>
2025-01-30 05:57:19 -0600

    yes

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -7,11 +7,27 @@ import '../styles/Projects.css';
 import '../styles/Skills.css';
 
 const homeSkills = [
-    { name: 'IBM Full Stack Software Developer Professional Certificate (V5)' },
-    { name: 'Generative AI Essentials for Software Developers' },
-    { name: 'DevOps Essentials' },
-    { name: 'Front-end Development with React V2' },
-    { name: 'Software Engineering Essentials' }
+  { name: 'DevOps Capstone Certificate' },
+  { name: 'IBM Full Stack Software Developer Professional Certificate' },
+  { name: 'Git and GitHub' },
+  { name: 'Node and Express' },
+  { name: 'Linux Commands & Shell Scripting' },
+  { name: 'DevOps' },
+  { name: 'Agile Development and Scrum' },
+  { name: 'Front-end Development with React' },
+  { name: 'Software Engineering' },
+  { name: 'HTML, CSS, & JavaScript' },
+  { name: 'Cloud Computing' },
+  { name: 'Python for Data Science and AI' },
+  { name: 'Python for AI and Application Development' },
+  { name: 'Developing Applications with SQL, Databases, and Django' },
+  { name: 'Container & Kubernetes' },
+  { name: 'Application Development using Microservices and Serverless' },
+  { name: 'Full Stack Application and Software Development' },
+  { name: 'Generative AI Essentials for Software Development' },
+  { name: 'Test Driven Development' },
+  { name: 'Monitoring and Observability for Development' },
+
 ];
 
 const projects = [
@@ -195,7 +211,7 @@ const Home = () => {
           <Container>
             <Row className="mb-4">
               <Col>
-                <h2 className="section-title">Key Certifications</h2>
+                <h2 className="section-title">Skills</h2>
                 <div className="skills-container">
                   {homeSkills.map((skill, index) => (
                     <div key={index} className="home-skill-item">
